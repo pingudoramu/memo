ee011c1b3169e1f2575e762549d044592fe593fb
pingudoramu <muyouwww@gmail.com>
2024-11-05 14:26:29 +0800

    xcode项目新建

---
+++ memo/memo/memoApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoApp.swift
+//  memo
+//
+//  Created by mac on 2024/11/5.
+//
+
+import SwiftUI
+
+@main
+struct memoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
